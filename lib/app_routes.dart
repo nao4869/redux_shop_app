4e72b394a56873abd61491c38860fa632fa81910
naohito muraoka <muraokan@oregonstate.edu>
2020-02-26 16:59:37 +0900

    app routes file added

---
+++ lib/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const home = "/";
+  static const listSchool = "/list-schools";
+  static const addSchool= "/add-school";
+  static const removeSchool = "/remove-school";
+  static const schoolDetail = "/school-detail";
+}
